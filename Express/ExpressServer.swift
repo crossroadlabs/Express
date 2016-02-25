5921dc6ab27d75d5e3ceac3ce0c6f255692e9a68
Daniel Leping <dileping@gmail.com>
2016-02-25 19:20:11 +0200

    added dispatch note

--- Express/ExpressServer.swift
+++ Express/ExpressServer.swift
@@ -31,6 +31,9 @@ public extension Express {
     }
     
     @noreturn func run() {
+        #if os(Linux) && !dispatch
+            print("Note: Express is currently running without dispatch support. We have implemented this mode to support Linux developers while Dispatch for Linux is not available out of the box. Consider it to be development mode only and not suitable for production as it might cause occasional hanging and crashes. Still, there is a possibility to build Express with dispatch support (recommended for production use). Follow this link for more info: https://github.com/crossroadlabs/Express")
+        #endif
         ExecutionContext.run()
     }
 }
